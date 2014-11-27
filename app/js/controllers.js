6ba35d1cd8c9d2ba8ea09f4d445562c0ca7cbf58
Oni Kehinde Omowunmi <Kehinde@Onis-MacBook-Pro-2.local>
2014-11-27 14:54:44 +0100

    fixed sendRequests error

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,22 +16,25 @@ angular.module("matsi.controllers", ['firebase', 'ngCookies'])
             $scope.submitFellow = function() {
                 FellowService.updateFellow($scope.fellowData, $rootScope.currentUser.uid);
             };
-            $scope.sendMail = function() {
-              // if (!$rootScope.currentUser) { 
-              //   alert("You Need to be logged in before you can mentor This fellow!!");
-              //   return false;
-              // };
-                var paramsFellow = angular.copy($scope.oneFellowData);
-                delete paramsFellow.$id;
-                delete paramsFellow.$priority;
-                console.log(paramsFellow, 'rackCity');
-                //var paramsMentor = MentorService.readSingleMentor();
-                $http.post('/mail/user/1', paramsFellow).success(function(r) {
-                    console.log(r);
-                });
-            };
-        }
-    ])
+    $scope.sendMail = function(){
+        var paramsFellow = angular.copy($scope.oneFellowData);
+        delete paramsFellow.$id;
+        delete paramsFellow.$priority;
+
+        console.log(paramsFellow,'rackCity');
+        //var paramsMentor = MentorService.readSingleMentor();
+        $http.post('/mail/user/1',paramsFellow).success(function(r){
+            console.log(r);
+        });
+        $scope.sendRequests();
+    };
+
+    $scope.sendRequests = function(){
+        FellowService.regRequest($rootScope.currentUser);
+    };
+
+
+}]) 
     .controller("MainCtrl", ['$rootScope', '$scope', '$firebase', '$cookies', 'FellowService',
         function($rootScope, $scope, $firebase, $cookies, FellowService) {
           
