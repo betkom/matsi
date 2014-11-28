ae1de114bb681a0a6358c724c9583bd69b90a479
Shittu Kehinde <shittu.adedamola@andela.co>
2014-11-28 10:14:35 +0100

    send mail function added

--- app/js/services.js
+++ app/js/services.js
@@ -1,64 +1,82 @@
-angular.module("matsi.services", ['firebase','ngCookies'])
+angular.module("matsi.services", ['firebase', 'ngCookies'])
+.factory('Authentication', ['$rootScope', '$cookies', function() {
 
-.factory('FellowService',['$firebase','$cookies', '$stateParams', function($firebase,$cookies,$stateParams){
-	var rootRef = new Firebase($cookies.rootRef);
-	// rootRef.__proto__.orderByChild = rootRef.__proto__.orderByChild || function(x){
-	// 	return this;
-	// };
-	// rootRef.__proto__.equalTo = rootRef.__proto__.equalTo || function(x){
-	// 	return this;
-	// };
-	// console.log($cookies);
-	return {
-		updateFellow:function(fellowData,currentUID){
-				console.log(currentUID);
-				var fellowData1 =angular.copy(fellowData);
-				console.log(fellowData1);
-				delete fellowData1.$$conf;
-				delete fellowData1.$priority;
-				delete fellowData1.$id;
-				delete fellowData1.__proto__;
-				console.log("final",fellowData1);
-				rootRef.child('users').child(currentUID).update(fellowData1);
-		},
-		readFellow: function(){
-		return $firebase(rootRef.child('users').orderByChild('role').equalTo('-fellow-')).$asObject();
-	},
-	readMyProfile: function(currentUID){
-		return $firebase(rootRef.child('users').child(currentUID)).$asObject();
-	},
-		readSingleFellow: function(){
-			if($stateParams.uid) {
-				return $firebase(rootRef.child('users').child($stateParams.uid)).$asObject();
-			}
-		}
-	};	
-}])
-.factory('MentorService', ['$firebase', '$cookies', '$stateParams',function($firebase,$cookies,$stateParams){
-	var rootRef = new Firebase($cookies.rootRef);
-	return {
-		readMentor: function(callback){ 
-			console.log($stateParams);
-			var mentors = $firebase(rootRef.child('users').orderByChild('role').equalTo('-mentor-')).$asArray();
-			if(callback && typeof callback === typeof function(){})
-				mentors.$loaded().then(callback);
-			return mentors;
-		},
-		readMyProfile: function(currentUID){
-			 return $firebase(rootRef.child('users').child(currentUID)).$asObject();
-			},
-		readSingleMentor: function(){
-			if($stateParams.uid){
-			 return $firebase(rootRef.child('users').child($stateParams.uid)).$asObject();
-			}
-		},
-		updateMentor: function(mentorData, currentUId){
-			mentorData1 = angular.copy(mentorData)
-			delete mentorData1.$$conf;
-			delete mentorData1.$id;
-			delete mentorData1.$priority;
-			delete mentorData1._proto_;
-			rootRef.child('users').child(currentUId).update(mentorData1);
-		}
-	};
-}]);
+            var rootRef = new Firebase($cookies.rootRef);
+            // Start with no user logged in
+            $rootScope.currentUser = null;
+            $rootScope.allowUser = false;
+            return {
+
+                login: function() {
+
+                },
+
+                logout: function() {
+
+                }
+            }
+            }])
+        .factory('FellowService', ['$firebase', '$cookies', '$stateParams', function($firebase, $cookies, $stateParams) {
+            var rootRef = new Firebase($cookies.rootRef);
+            // rootRef.__proto__.orderByChild = rootRef.__proto__.orderByChild || function(x){
+            // 	return this;
+            // };
+            // rootRef.__proto__.equalTo = rootRef.__proto__.equalTo || function(x){
+            // 	return this;
+            // };
+            // console.log($cookies);
+            return {
+                updateFellow: function(fellowData, currentUID) {
+                    console.log(currentUID);
+
+                    var fellowData1 = angular.copy(fellowData);
+                    console.log(fellowData1);
+                    delete fellowData1.$$conf;
+                    delete fellowData1.$priority;
+                    delete fellowData1.$id;
+                    delete fellowData1.__proto__;
+                    console.log("final", fellowData1);
+                    rootRef.child('users').child(currentUID).update(fellowData1);
+                },
+                readFellow: function() {
+                    return $firebase(rootRef.child('users').orderByChild('role').equalTo('-fellow-')).$asObject();
+                },
+                readMyProfile: function(currentUID) {
+                    return $firebase(rootRef.child('users').child(currentUID)).$asObject();
+                },
+                readSingleFellow: function() {
+                    if ($stateParams.uid) {
+                        return $firebase(rootRef.child('users').child($stateParams.uid)).$asObject();
+                    }
+                }
+            };
+        }])
+        .factory('MentorService', ['$firebase', '$cookies', '$stateParams', function($firebase, $cookies, $stateParams) {
+            var rootRef = new Firebase($cookies.rootRef);
+            return {
+                readMentor: function(callback) {
+                    console.log($stateParams);
+                    var mentors = $firebase(rootRef.child('users').orderByChild('role').equalTo('-mentor-')).$asArray();
+                    if (callback && typeof callback === typeof
+                        function() {})
+                        mentors.$loaded().then(callback);
+                    return mentors;
+                },
+                readMyProfile: function(currentUID) {
+                    return $firebase(rootRef.child('users').child(currentUID)).$asObject();
+                },
+                readSingleMentor: function() {
+                    if ($stateParams.uid) {
+                        return $firebase(rootRef.child('users').child($stateParams.uid)).$asObject();
+                    }
+                },
+                updateMentor: function(mentorData, currentUId) {
+                    mentorData1 = angular.copy(mentorData)
+                    delete mentorData1.$$conf;
+                    delete mentorData1.$id;
+                    delete mentorData1.$priority;
+                    delete mentorData1._proto_;
+                    rootRef.child('users').child(currentUId).update(mentorData1);
+                }
+            };
+        }]);
