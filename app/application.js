1e6f6b6e4ff41e2dd35bd5e3271107331fa8f706
Terwase Kelvin Gberikon <terwase@live.com>
2014-12-04 16:12:36 +0100

    Refactored the config section from application.js and added to a config folder and config.js file

--- app/application.js
+++ app/application.js
@@ -1,17 +1,9 @@
+require("./js/config/config.js");
 require("./js/controllers/MentorController.js");
 require("./js/controllers/FellowController.js");
 require("./js/directives/mentorRequest.js");
 require("./js/directives/header.js");
 require("./js/services.js");
-
-window.Matsi = angular.module("Matsi", [
-    'matsi.controllers',
-    'matsi.services',
-    'matsi.directives',
-    'ngAnimate',
-    'ngMaterial',
-    'ui.router'
-]);
 
 Matsi.run(['$rootScope', function($rootScope) {
     // set globals we want available in ng expressions
@@ -22,89 +14,6 @@ Matsi.run(['$rootScope', function($rootScope) {
 if (window.location.toString().indexOf('#&__firebase_request_key') > -1) {
     window.location = '/';
 }
-
-Matsi.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $rootScope) {
-
-    $locationProvider.html5Mode(true);
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'pages/home.html',
-            controller: '',
-            data: {
-                access: 'public'
-            }
-        })
-        .state('updateFellowProfile', {
-            url: '/updateFellowProfile',
-            templateUrl: 'pages/updateFellowProfile.html',
-            controller: 'FellowController',
-            
-            data: {
-                access: 'private',
-                authorizedRoles: [false],
-                authenticate: true
-            }
-        })
-        .state('myProfile', {
-            url: '/myProfile',
-            templateUrl: 'pages/myProfile.html',
-            controller: '',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('fellowProfile', {
-            url: '/fellowProfile/:uid',
-            templateUrl: 'pages/fellowProfile.html',
-            controller: 'FellowController',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('fellows', {
-            url: '/fellows',
-            templateUrl: 'pages/fellows.html',
-            controller: 'FellowController',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('editMentor', {
-            url: '/editMentor',
-            templateUrl: 'pages/editMentor.html',
-            controller: 'MentorController',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('findOneMentor', {
-            url: '/mentors/:uid',
-            templateUrl: 'pages/viewMentor.html',
-            controller: 'MentorController',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('findAllMentors', {
-            url: '/mentors',
-            templateUrl: 'pages/viewAllMentors.html',
-            controller: 'MentorController',
-            data: {
-                access: 'private'
-            }
-        })
-        .state('settings', {
-            url: '/settings',
-            templateUrl: 'pages/settings.html',
-            controller: 'SettingsController',
-            data: {
-                access: 'private'
-            }
-        });
-        $urlRouterProvider.otherwise("/");
-
-}]);
 
 window.escapeEmailAddress = function(email) {
     if (!email) {
