4a3ddd450a6369eaf102fbc28ce521e82a15c77e
andela-tkomolafe <betkomuniq@yahoo.com>
2014-12-09 15:16:36 +0100

    Configured services tests

--- app/js/services/refs.js
+++ app/js/services/refs.js
@@ -1,8 +1,7 @@
 angular.module("matsi.services")
     .factory('Refs', ['$firebase', '$cookies', '$rootScope', function($firebase, $cookies, $rootScope) {
-        var rootRef = new Firebase($cookies.rootRef);
+        var rootRef = new Firebase($cookies.rootRef || 'https://brilliant-heat-9512.firebaseio.com/');
         return {
-
           rootRef: rootRef,
           userRef: rootRef.child('users')
         };
--- app/test/specs.js
+++ app/test/specs.js
@@ -1,67 +1,69 @@
-describe('Testing 001', function(){
-	var FellowService,
-		scope,
-		MentorService,
-		$httpBackend,
-		$stateParams,
-		$location;
-	beforeEach(module("Matsi"));
-	beforeEach(inject(function($injector, $controller, $rootScope, $cookies, _$location_, _$stateParams_, _$httpBackend_) {
-			// Set a new global scope
-			$cookies.rootRef = 'https://brilliant-heat-9512.firebaseio.com/';
-			scope = $rootScope;
-			// Point global var
-			$stateParams = _$stateParams_;
-			$httpBackend = _$httpBackend_;
-			//$location = _$location_;
-			FellowService = $injector.get('FellowService');
-			MentorService = $injector.get('MentorService');
-		}));
-	it('should return all fellows as object', function(){
-		var fellowData = FellowService.readFellow();
-		expect(fellowData.$id).toBeDefined();
-		expect(typeof fellowData).toEqual('object');
+describe('Fellow Mentor Service Test',function(){
+
+	var FellowService, 
+	MentorService, 
+	MailService,
+	Refs,
+	mockFellow = {
+		uid: 'happy-fellow-id',
+		fullName: 'Happy Fellow',
+		role: '-fellow-',
+		email: 'happy-fellow-id@andela.co'
+	},
+	mockMentor = {
+		uid: 'happy-mentor-id',
+		role: '-mentor-',
+		fullName: 'Happy Mentor',
+		email: 'happy-mentor-id@andela.co'
+	};
+
+	beforeEach(function(){
+		module('Matsi');
 	});
 
-	it("should return current user profile",function(){
-		var uid = "google:100424084914655768945";
-		var data = FellowService.readSingleFellow(uid);
-		console.log(data);
-		expect(data.$id).toEqual(uid);
+	beforeEach(inject(function($injector){
+		FellowService = $injector.get('FellowService');
+		MentorService = $injector.get('MentorService');
+		MailService = $injector.get('MailService');
+		Refs = $injector.get('Refs');
+	}));
+
+	beforeEach(function(done){
+		Refs.rootRef.child('users').child(mockMentor.uid).set(mockMentor,function(err){
+			Refs.rootRef.child('users').child(mockFellow.uid).set(mockFellow,function(err){
+				done();
+			});	
+		});
 	});
 
-	it('Should update fellow', function(){
-		var data = {};
-		data.uid = "google:100424084914655768945";
-		FellowService.updateFellow(data,onComplete);
-		function onComplete(err){
-			if(err){
-				console.log("this is err",err);
-				return false;
-			}
-			else{
-				return true;
-			}
-		}
-		expect(onComplete).toBeTruthy();
+	describe('Mentors and Fellow Relationship',function(){
+		
+			it('Should have created the Fellow',function(done){
+				FellowService.findOne(mockFellow.uid,function(snap){
+					var fellow = snap.val();
+					expect(fellow.uid).toBe(mockFellow.uid);
+					done();
+				});
+			});
+
+			it('Should have created the Mentor',function(done){
+				MentorService.findOne(mockMentor.uid,function(snap){
+					var mentor = snap.val();
+					expect(mentor.uid).toBe(mockMentor.uid);
+					done();
+				});
+			});
+
 	});
 
-	it('should return all mentors as object', function(){
-		var mentorData = MentorService.readMentor();
-		expect(mentorData.$id).toBeDefined();
-		expect(typeof mentorData).toEqual('object');
+	afterEach(function(done){
+		Refs.rootRef.child('users').child(mockMentor.uid).remove(function (err) {
+				expect(err).toBe(null);
+				Refs.rootRef.child('users').child(mockFellow.uid).remove(function(err){	
+					expect(err).toBe(null);
+					done();
+				});					
+		});
 	});
-	
-	// var mentor = {
-	// 	name: 'kenny',
-	// 	email: 'kenny@yahoo.com',
-	// 	uid: '872862926098'
-	// };
 
-	// it('should update a mentor', function(){
-	// 	MentorService.updateMentor(mentor, mentor.uid);
-	// 	expect(mentor.name).not.toBeUndefined();
-	// 	expect(mentor.name).toEqual('kenny');
-	// 	expect(mentor.email).toMatch(/*.\@\.*/);
-	// });
 });
