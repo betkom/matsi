b3d77857a1750d901e892dc11f01be5043e7049f
andela-tkomolafe <betkomuniq@yahoo.com>
2014-12-09 14:52:21 +0100

    added refs.js to stage

---
+++ app/js/services/refs.js
@@ -0,0 +1,9 @@
+angular.module("matsi.services")
+    .factory('Refs', ['$firebase', '$cookies', '$rootScope', function($firebase, $cookies, $rootScope) {
+        var rootRef = new Firebase($cookies.rootRef);
+        return {
+
+          rootRef: rootRef,
+          userRef: rootRef.child('users')
+        };
+    }]);
