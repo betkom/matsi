4343551a51a722afc155397c8b7f880b92acbd83
Shittu Kehinde <shittu.adedamola@andela.co>
2014-12-10 08:52:21 +0100

    added test cases

--- app/test/specs.js
+++ app/test/specs.js
@@ -1,69 +1,81 @@
-describe('Fellow Mentor Service Test',function(){
+describe('Fellow Mentor Service Test', function() {
 
-	var FellowService, 
-	MentorService, 
-	MailService,
-	Refs,
-	mockFellow = {
-		uid: 'happy-fellow-id',
-		fullName: 'Happy Fellow',
-		role: '-fellow-',
-		email: 'happy-fellow-id@andela.co'
-	},
-	mockMentor = {
-		uid: 'happy-mentor-id',
-		role: '-mentor-',
-		fullName: 'Happy Mentor',
-		email: 'happy-mentor-id@andela.co'
-	};
+    var FellowService,
+        MentorService,
+        MailService,
+        Refs,
+        mockFellow = {
+            uid: 'happy-fellow-id',
+            fullName: 'Happy Fellow',
+            role: '-fellow-',
+            email: 'happy-fellow-id@andela.co'
+        },
 
-	beforeEach(function(){
-		module('Matsi');
-	});
+        mockMentor = {
+            uid: 'happy-mentor-id',
+            role: '-mentor-',
+            fullName: 'Happy Mentor',
+            email: 'happy-mentor-id@andela.co'
+        };
 
-	beforeEach(inject(function($injector){
-		FellowService = $injector.get('FellowService');
-		MentorService = $injector.get('MentorService');
-		MailService = $injector.get('MailService');
-		Refs = $injector.get('Refs');
-	}));
+    beforeEach(function() {
+        module('Matsi');
+    });
 
-	beforeEach(function(done){
-		Refs.rootRef.child('users').child(mockMentor.uid).set(mockMentor,function(err){
-			Refs.rootRef.child('users').child(mockFellow.uid).set(mockFellow,function(err){
-				done();
-			});	
-		});
-	});
+    beforeEach(inject(function($injector) {
+        FellowService = $injector.get('FellowService');
+        MentorService = $injector.get('MentorService');
+        MailService = $injector.get('MailService');
+        Refs = $injector.get('Refs');
+    }));
 
-	describe('Mentors and Fellow Relationship',function(){
-		
-			it('Should have created the Fellow',function(done){
-				FellowService.findOne(mockFellow.uid,function(snap){
-					var fellow = snap.val();
-					expect(fellow.uid).toBe(mockFellow.uid);
-					done();
-				});
-			});
+    beforeEach(function(done) {
+        Refs.rootRef.child('users').child(mockMentor.uid).set(mockMentor, function(err) {
+            Refs.rootRef.child('users').child(mockFellow.uid).set(mockFellow, function(err) {
+                done();
+            });
+        });
+    });
 
-			it('Should have created the Mentor',function(done){
-				MentorService.findOne(mockMentor.uid,function(snap){
-					var mentor = snap.val();
-					expect(mentor.uid).toBe(mockMentor.uid);
-					done();
-				});
-			});
+    describe('Mentors and Fellow Relationship', function() {
 
-	});
+        it('Should have created the Fellow', function(done) {
+            FellowService.findOne(mockFellow.uid, function(snap) {
+                var fellow = snap.val();
+                expect(fellow.uid).toBe(mockFellow.uid);
+                done();
+            });
+        });
 
-	afterEach(function(done){
-		Refs.rootRef.child('users').child(mockMentor.uid).remove(function (err) {
-				expect(err).toBe(null);
-				Refs.rootRef.child('users').child(mockFellow.uid).remove(function(err){	
-					expect(err).toBe(null);
-					done();
-				});					
-		});
-	});
+        // it('should update mockFellow', function(done) {
+        // 	mockFellow.name = 'This is a Mock Name';
+        //     FellowService.update(mockFellow,function(snap){
+        //     	var fellow = snap.val();
+        //     	expect(mockFellow.name).toBe(fellow.name);
+        //     	done();
+        //     });
+        // });
+
+        it('Should have created the Mentor', function(done) {
+            MentorService.findOne(mockMentor.uid, function(snap) {
+                var mentor = snap.val();
+                expect(mentor.uid).toBe(mockMentor.uid);
+                done();
+            });
+        });
+
+    });
+
+
+
+    afterEach(function(done) {
+        Refs.rootRef.child('users').child(mockMentor.uid).remove(function(err) {
+            expect(err).toBe(null);
+            Refs.rootRef.child('users').child(mockFellow.uid).remove(function(err) {
+                expect(err).toBe(null);
+                done();
+            });
+        });
+    });
 
 });
