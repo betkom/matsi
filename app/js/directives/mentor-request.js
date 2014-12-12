85fa6dd508488e4f03278ca4c7ff2ec9c06cd5cb
Adeleke Togun <adeleketogun@gmail.com>
2014-12-12 13:14:37 +0100

    Implemented Toast as a service

--- app/js/directives/mentor-request.js
+++ app/js/directives/mentor-request.js
@@ -3,26 +3,25 @@ angular.module("matsi.directives")
         return {
             restrict: 'E',
             templateUrl: '/pages/mentor-request.html',
-            controller: ['$scope', '$http', 'Mentor', 'MailService', 'Fellow', function($scope, $http, MentorService, MailService, FellowService) {
-                
+            controller: ['$scope', '$http', 'Mentor', 'MailService', 'Fellow', 'utils', function($scope, $http, MentorService, MailService, Fellow, utils) {
+
                 $scope.mentor = MentorService.findOne($scope.mentor_uid);
                 $scope.showMessageBox = false;
-                
+
                 $scope.accept = function() {
                     console.log('blue ribbon');
-                    FellowService.accept($scope.mentor);
-                    MailService.send(3,$scope.mentor);
-                };
+                    Fellow.accept($scope.mentor);
+                    MailService.send(3, $scope.mentor);
+                    utils.openToast('Request Accepted')
 
-                $scope.openToast = function($event) {
-                    console.log('*********** red ribbon');
-                    $mdToast.show($mdToast.simple().content('Request Accepted'));
                 };
 
                 $scope.reject = function() {
-                    FellowService.reject($scope.mentor);
-                    MailService.send(4,$scope.mentor);
+                    Fellow.reject($scope.mentor);
+                    MailService.send(4, $scope.mentor);
                     $scope.showMessageBox = false;
+                    utils.openToast('Request Rejected')
+
                 };
 
                 $scope.showBox = function() {
