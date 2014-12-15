02318c0d5cabc28c4e086e8012a7f582e10adbb1
Andela-Kshittu <shittu.adedamola@andela.co>
2014-12-15 18:07:34 +0100

    smarterer api integration

--- app/js/services/exports/utils.js
+++ app/js/services/exports/utils.js
@@ -1,16 +1,16 @@
-module.exports = function($rootScope, $mdToast, $mdDialog){
-	return {
+module.exports = function($rootScope, $mdToast, $mdDialog) {
+    return {
         openToast: function(message) {
-            $mdToast.show($mdToast.simple().content(message));
+           $mdToast.show($mdToast.simple().content(message));
         },
-        showAlert: function(ev,message){
-			$mdDialog.show(
-			    $mdDialog.alert()
-			    .title('Oops, request not sent!!!')
-			    .content(message)
-			    .ok('Okay!')
-			    .targetEvent(ev)
-			);
+      showAlert: function(ev, message) {
+          $mdDialog.show(
+            $mdDialog.alert()
+            .title('Oops, request not sent!!!')
+            .content(message)
+            .ok('Okay!')
+            .targetEvent(ev)
+          );
         }
-	};
+    };
 };
